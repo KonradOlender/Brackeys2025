49ce2ae75a3b5a1c9ec3449ac6f14a77e694c7cf
KonradOlender <50641019+KonradOlender@users.noreply.github.com>
2025-02-18 18:48:01 +0100

    jump code

--- brackeys2025-godot/Player/player.gd
+++ brackeys2025-godot/Player/player.gd
@@ -3,22 +3,38 @@ extends CharacterBody2D
 @export var speed := 350
 const FLOATY_GRAVITY := 100
 
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_ground : float
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_ground * jump_time_to_ground)) * -1.0
+var is_jumping: bool = false
+
+
 func _physics_process(delta: float) -> void:
-	var direction := Vector2(0,0)
-	
 	
 	#actual momvement
+	var direction := Vector2(0,0)
 	direction.x = 1 if Input.is_action_pressed("move_right") else 0
-	direction.y = Input.get_axis("move_up", "move_down")
+	velocity.x = direction.x * speed 
 	
-	velocity = direction * speed 
+	#jumping
+	if !Input.is_action_pressed("jump"):
+		velocity.y += get_gravity_fall() * delta
+	velocity.y += get_gravity_jump() * delta
 	
-	if not is_on_floor():
-		velocity.y += FLOATY_GRAVITY
-	else:
-		if Input.is_action_just_pressed("jump"):
-			velocity.y = -5000
-			
-	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump()
 	
 	move_and_slide()
+	
+func get_gravity_jump() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+func get_gravity_fall() -> float:
+	return fall_gravity
+	
+func jump():
+	is_jumping = true
+	velocity.y = jump_velocity
