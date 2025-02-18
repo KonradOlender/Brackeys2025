4062a21489395698a0a2f6cae2c119829936d9c9
czarekdupa <150566219+czarekdupa@users.noreply.github.com>
2025-02-18 16:46:36 -0600

    fixed jump animation timing and added platform for level 1

--- brackeys2025-godot/Levels/animated_glove.gd
+++ brackeys2025-godot/Levels/animated_glove.gd
@@ -7,5 +7,5 @@ func _process(delta: float) -> void:
 	elif $"..".is_on_floor():
 		play("idle")
 	
-	if Input.is_action_pressed("jump") and $"..".is_on_floor():
+	if Input.is_action_pressed("jump") and not $"..".is_on_floor():
 		play("jump")
